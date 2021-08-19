2fd2c859c4cfce50171e82803dc9fab7422034c9
jaewan0114 <42615916+jaewan0114@users.noreply.github.com>
2021-08-19 20:39:23 +0900

    Ready for deploy

---
+++ quiz/views.py
@@ -0,0 +1,18 @@
+from django.db.models import manager
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QuizSerializer
+import random
+
+# Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello world")
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuiz = random.sample(list(totalQuizs),id)
+    Serializer = QuizSerializer(randomQuiz, many=True)
+    return Response(Serializer.data)
